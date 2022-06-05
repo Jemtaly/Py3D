2a8d8501b7bdb8590dde134a9556b1318835505d
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2022-06-06 00:06:32 +0800

    Update tk3d.py

--- tk3d.py
+++ tk3d.py
@@ -4,23 +4,25 @@ class Space:
     def __init__(self, points={}, lines=set()):
         self.points = points
         self.lines = lines
-        self.camera = numpy.array([0., 0., 0.])
-        self.matrix = numpy.array([[1., 0., 0.], [0., -1., 0.], [0., 0., -1.]])
-        self.scale = 400.
-        self.stick = 100.
+        self.camera = numpy.array([0.0, 0.0, 0.0])
+        self.matrix = numpy.array([[1.0, 0.0, 0.0], [0.0, -1.0, 0.0], [0.0, 0.0, -1.0]])
+        self.scale = 720.0
+        self.stick = 120.0
     def start(self):
         self.tk = tkinter.Tk()
         self.canvas = tkinter.Canvas(self.tk)
         self.canvas.pack(fill=tkinter.BOTH, expand=True)
-        self.canvas.bind('<ButtonPress-1>', self.turn_press)
+        self.canvas.bind('<Button-1>', self.turn_press)
         self.canvas.bind('<B1-Motion>', self.turn_motion)
-        self.canvas.bind('<ButtonPress-3>', self.tilt_press)
+        self.canvas.bind('<Button-3>', self.tilt_press)
         self.canvas.bind('<B3-Motion>', self.tilt_motion)
-        self.canvas.bind('<ButtonPress-2>', self.move_press)
+        self.canvas.bind('<Button-2>', self.move_press)
         self.canvas.bind('<B2-Motion>', self.move_motion)
         self.canvas.bind('<MouseWheel>', self.move_wheel)
-        self.tk.bind('=', self.scale_up)
-        self.tk.bind('-', self.scale_down)
+        self.canvas.bind('<Button-4>', self.move_wheel)
+        self.canvas.bind('<Button-5>', self.move_wheel)
+        self.tk.bind('=', self.zoom_scale)
+        self.tk.bind('-', self.zoom_scale)
         self.canvas.bind('<Configure>', self.configure)
         self.tk.mainloop()
     def refresh(self):
@@ -30,7 +32,7 @@ class Space:
             if relative[2] > 0:
                 relatives[k] = relative[0] / relative[2] * self.scale + self.center_x, relative[1] / relative[2] * self.scale + self.center_y
         self.canvas.delete(tkinter.ALL)
-        self.canvas.create_text(0., 0., fill='blue', text='Scale = {}'.format(self.scale), anchor=tkinter.NW)
+        self.canvas.create_text(0.0, 0.0, fill='blue', text='Scale = {}'.format(self.scale), anchor=tkinter.NW)
         # for k, coordinate in relatives.items():
         #     self.canvas.create_text(*coordinate, fill='blue', text=k)
         for p, q in self.lines:
@@ -39,11 +41,12 @@ class Space:
     def turn_press(self, event):
         self.turn_rec = event
     def turn_motion(self, event):
-        s = math.sqrt((event.x - self.turn_rec.x) ** 2 + (event.y - self.turn_rec.y) ** 2)
-        nx, ny = (self.turn_rec.y - event.y) / s, (event.x - self.turn_rec.x) / s
-        alpha = s / self.scale
-        n = numpy.array([[nx, ny, 0.]])
-        N = numpy.array([[0., 0., ny], [0., 0., -nx], [-ny, nx, 0.]])
+        dx, dy = event.x - self.turn_rec.x, event.y - self.turn_rec.y
+        ds = math.sqrt(dx ** 2 + dy ** 2)
+        nx, ny = -dy / ds, dx / ds
+        alpha = ds / self.scale
+        n = numpy.array([[nx, ny, 0.0]])
+        N = numpy.array([[0.0, 0.0, ny], [0.0, 0.0, -nx], [-ny, nx, 0.0]])
         self.matrix = (math.cos(alpha) * numpy.eye(3) + (1 - math.cos(alpha)) * n * n.T + math.sin(alpha) * N).dot(self.matrix)
         self.refresh()
         self.turn_rec = event
@@ -51,34 +54,30 @@ class Space:
         self.tilt_rec = event
     def tilt_motion(self, event):
         ax, ay, bx, by = event.x - self.center_x, event.y - self.center_y, self.tilt_rec.x - self.center_x, self.tilt_rec.y - self.center_y
-        di = (math.sqrt(ax ** 2 + ay ** 2) * math.sqrt(bx ** 2 + by ** 2))
-        sina, cosa = (ax * by - ay * bx) / di, (ax * bx + ay * by) / di
-        self.matrix = numpy.array([[cosa, sina, 0.], [-sina, cosa, 0.], [0., 0., 1.]]).dot(self.matrix)
+        asbs = math.sqrt(ax ** 2 + ay ** 2) * math.sqrt(bx ** 2 + by ** 2)
+        sina, cosa = (ax * by - ay * bx) / asbs, (ax * bx + ay * by) / asbs
+        self.matrix = numpy.array([[cosa, sina, 0.0], [-sina, cosa, 0.0], [0.0, 0.0, 1.0]]).dot(self.matrix)
         self.refresh()
         self.tilt_rec = event
     def move_press(self, event):
         self.move_rec = event
     def move_motion(self, event):
-        self.camera -= numpy.linalg.inv(self.matrix).dot(numpy.array([(event.x - self.move_rec.x) / self.stick, (event.y - self.move_rec.y) / self.stick, 0.]))
+        self.camera -= numpy.linalg.inv(self.matrix).dot(numpy.array([(event.x - self.move_rec.x) / self.stick, (event.y - self.move_rec.y) / self.stick, 0.0]))
         self.refresh()
         self.move_rec = event
     def move_wheel(self, event):
-        self.camera -= numpy.linalg.inv(self.matrix).dot(numpy.array([0., 0., (.5 - event.delta) / self.stick]))
+        self.camera -= numpy.linalg.inv(self.matrix).dot(numpy.array([0.0, 0.0, (-event.delta or event.num * 240 - 1080) / self.stick]))
         self.refresh()
-    def scale_up(self, event):
-        self.scale *= 1.05
-        self.refresh()
-    def scale_down(self, event):
-        self.scale /= 1.05
+    def zoom_scale(self, event):
+        self.scale *= event.keycode / (41 - event.keycode)
         self.refresh()
     def configure(self, event):
-        self.center_y = .5 * event.height
-        self.center_x = .5 * event.width
+        self.center_y, self.center_x = 0.5 * event.height, 0.5 * event.width
         self.refresh()
 def main():
     import argparse, sys
     parser = argparse.ArgumentParser()
-    parser.add_argument('file', type=argparse.FileType('r'), default=sys.stdin.buffer)
+    parser.add_argument('file', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
     args = parser.parse_args()
     ps = {}
     ls = set()
