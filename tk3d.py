894f0f2b52964c4501b8338225c50dc5080ac344
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2022-06-08 13:18:42 +0800

    Update tk3d.py

--- tk3d.py
+++ tk3d.py
@@ -4,24 +4,24 @@ class Camera:
     def __init__(self):
         self.position = numpy.array([0.0, 0.0, 0.0])
         self.matrix = numpy.array([[1.0, 0.0, 0.0], [0.0, -1.0, 0.0], [0.0, 0.0, -1.0]])
-    def shoot(self, coordinate):
-        return self.matrix.dot(coordinate - self.position)
-    def move(self, dx, dy, dz):
-        self.position -= numpy.linalg.inv(self.matrix).dot(numpy.array([dx, dy, dz]))
-    def rotate(self, ax, ay, az):
-        alpha = math.sqrt(ax ** 2 + ay ** 2 + az ** 2)
-        if alpha > 0:
-            nx, ny, nz = ax / alpha, ay / alpha, az / alpha
+    def transform(self, absolute):
+        return self.matrix.dot(absolute - self.position)
+    def move(self, ds):
+        self.position -= numpy.linalg.inv(self.matrix).dot(ds)
+    def rotate(self, th):
+        alpha = numpy.linalg.norm(th)
+        if alpha:
+            nx, ny, nz = th / alpha
             n = numpy.array([[nx, ny, nz]])
-            N = numpy.array([[0.0, nz, ny], [-nz, 0.0, -nx], [-ny, nx, 0.0]])
+            N = numpy.array([[0.0, -nz, ny], [nz, 0.0, -nx], [-ny, nx, 0.0]])
             self.matrix = (math.cos(alpha) * numpy.eye(3) + (1 - math.cos(alpha)) * n * n.T + math.sin(alpha) * N).dot(self.matrix)
 class Space:
     def __init__(self, points={}, lines=set(), scale=120.0, distance=720.0):
         self.points = points
         self.lines = lines
+        self.camera = Camera()
         self.scale = scale # number of pixels corresponding to each unit length in the space
         self.distance = distance # number of pixels between the viewpoint and the projection plane
-        self.camera = Camera()
     def start(self):
         self.tk = tkinter.Tk()
         self.canvas = tkinter.Canvas(self.tk)
@@ -41,19 +41,19 @@ class Space:
         self.canvas.bind('<Configure>', self.configure)
         self.tk.mainloop()
     def refresh(self):
-        relatives = {}
-        for k, coordinate in self.points.items():
-            relative = self.camera.shoot(coordinate)
+        projections = {}
+        for k, absolute in self.points.items():
+            relative = self.camera.transform(absolute)
             if relative[2] > 0:
-                relatives[k] = relative[0] / relative[2] * self.distance + self.center_x, relative[1] / relative[2] * self.distance + self.center_y
+                projections[k] = self.cx + relative[0] / relative[2] * self.distance, self.cy + relative[1] / relative[2] * self.distance
         self.canvas.delete(tkinter.ALL)
         self.canvas.create_text(0.0, 0.0, fill='blue', text='Scale = {:.2f} px'.format(self.scale), anchor=tkinter.NW)
         self.canvas.create_text(0.0, 20.0, fill='blue', text='Distance = {:.2f} px'.format(self.distance), anchor=tkinter.NW)
         # for k, coordinate in relatives.items():
         #     self.canvas.create_text(*coordinate, fill='blue', text=k)
         for p, q in self.lines:
-            if p in relatives and q in relatives:
-                self.canvas.create_line(*relatives[p], *relatives[q])
+            if p in projections and q in projections:
+                self.canvas.create_line(*projections[p], *projections[q])
     def move_press(self, event):
         self.move_evrec = event
     def turn_press(self, event):
@@ -69,26 +69,26 @@ class Space:
     def motion(self, event):
         if event.state & 0x200: # move
             dx, dy = event.x - self.move_evrec.x, event.y - self.move_evrec.y
-            self.camera.move(dx / self.scale, dy / self.scale, 0.0)
+            self.camera.move(numpy.array([dx / self.scale, dy / self.scale, 0.0]))
             self.move_evrec = event
         if event.state & 0x100: # turn
             dx, dy = event.x - self.turn_evrec.x, event.y - self.turn_evrec.y
-            self.camera.rotate(-dy / self.distance, dx / self.distance, 0.0)
+            self.camera.rotate(numpy.array([-dy / self.distance, dx / self.distance, 0.0]))
             self.turn_evrec = event
         if event.state & 0x400: # tilt
-            ax, ay, bx, by = event.x - self.center_x, event.y - self.center_y, self.tilt_evrec.x - self.center_x, self.tilt_evrec.y - self.center_y
-            self.camera.rotate(0.0, 0.0, math.atan((ax * by - ay * bx) / (ax * bx + ay * by)))
+            bx, by, ax, ay = event.x - self.cx, event.y - self.cy, self.tilt_evrec.x - self.cx, self.tilt_evrec.y - self.cy
+            self.camera.rotate(numpy.array([0.0, 0.0, math.atan2(ax * by - ay * bx, ax * bx + ay * by)]))
             self.tilt_evrec = event
         self.refresh()
     def wheel(self, event):
         delta = event.delta or 1080 - event.num * 240
         if event.state & 0x200: # move forward/backward
-            self.camera.move(0.0, 0.0, -delta / self.scale)
+            self.camera.move(numpy.array([0.0, 0.0, -delta / self.scale]))
         else: # zoom in/out
             self.distance = (4800 + delta) / (4800 - delta) * self.distance
         self.refresh()
     def configure(self, event):
-        self.center_y, self.center_x = 0.5 * event.height, 0.5 * event.width
+        self.cx, self.cy = 0.5 * event.width, 0.5 * event.height
         self.refresh()
 def main():
     import argparse, sys
