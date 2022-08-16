eaaf26f398f5d6fd184b067cb0a457706ec97c35
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2022-08-16 13:08:36 +0800

    Add files via upload

--- tk3d.py
+++ tk3d.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import tkinter, numpy, math, copy
 class Camvas(tkinter.Canvas):
-    def __init__(self, master, verts = {}, lines = set()):
+    def __init__(self, master, verts = {}, lines = set(), centre = numpy.zeros(3), matrix = numpy.eye(3), dist = 960.0, size = 160.0):
+        assert numpy.allclose(matrix @ matrix.T, numpy.eye(3))
+        assert dist in numpy.arange(600.0, 6000.0, 60.0) or dist == 6000.0
+        assert size in numpy.arange(100.0, 1000.0, 10.0) or size == 1000.0
         super().__init__(master)
         self.bind('<ButtonPress-1>', self.turn_start)
         self.bind('<ButtonPress-3>', self.tilt_start)
@@ -18,16 +21,16 @@ class Camvas(tkinter.Canvas):
         self.bind('<Configure>', self.config)
         self.frame = tkinter.Frame(self)
         self.frame.place(relx = 1.0, rely = 0.0, anchor = tkinter.NE)
-        self.dist_var = tkinter.DoubleVar(value = 960.0) # number of pixels between the viewpoint and the projection plane
-        self.size_var = tkinter.DoubleVar(value = 160.0) # number of pixels corresponding to each unit length in the space
+        self.dist_var = tkinter.DoubleVar(value = dist) # number of pixels between the viewpoint and the projection plane
+        self.size_var = tkinter.DoubleVar(value = size) # number of pixels corresponding to each unit length in the space
         self.dist_scaler = tkinter.Scale(self.frame, from_ = 600.0, to = 6000.0, resolution = 60.0, length = 180, variable = self.dist_var, orient = tkinter.HORIZONTAL, label = 'Dist', command = self.dist_change)
         self.size_scaler = tkinter.Scale(self.frame, from_ = 100.0, to = 1000.0, resolution = 10.0, length = 180, variable = self.size_var, orient = tkinter.HORIZONTAL, label = 'Size', command = self.size_change)
         self.dist_scaler.pack()
         self.size_scaler.pack()
         self.dist = self.dist_var.get()
         self.size = self.size_var.get()
-        self.centre = numpy.zeros(3)
-        self.matrix = numpy.eye(3)
+        self.centre = centre.copy()
+        self.matrix = matrix.copy()
         self.verts = copy.deepcopy(verts)
         self.lines = copy.deepcopy(lines)
     def refresh(self):
@@ -37,8 +40,8 @@ class Camvas(tkinter.Canvas):
             if relative[2] < 0:
                 positions[k] = self.cx - relative[0] / relative[2] * self.dist, self.cy + relative[1] / relative[2] * self.dist
         self.delete(tkinter.ALL)
-        # for k, centre in positions.items():
-        #     self.create_text(*centre, fill = 'blue', text = k)
+        # for k, position in positions.items():
+        #     self.create_text(*position, fill = 'blue', text = k)
         for p, q in self.lines:
             if p in positions and q in positions:
                 self.create_line(*positions[p], *positions[q])
@@ -69,9 +72,6 @@ class Camvas(tkinter.Canvas):
     def wheel(self, event):
         mvz = (0 - event.delta or event.num * 240 - 1080) / self.size
         self.move(numpy.array([0.0, 0.0, mvz]))
-    def move(self, mv):
-        self.centre += numpy.linalg.inv(self.matrix).dot(mv)
-        self.refresh()
     def rota(self, rt):
         alpha = numpy.linalg.norm(rt)
         if alpha:
@@ -80,14 +80,17 @@ class Camvas(tkinter.Canvas):
             N = numpy.array([[0.0, -nz, +ny], [+nz, 0.0, -nx], [-ny, +nx, 0.0]])
             self.matrix = (math.cos(alpha) * numpy.eye(3) + (1 - math.cos(alpha)) * n * n.T + math.sin(alpha) * N).dot(self.matrix)
         self.refresh()
-    def config(self, event):
-        self.cx, self.cy = event.width / 2, event.height / 2
+    def move(self, mv):
+        self.centre += numpy.linalg.inv(self.matrix).dot(mv)
         self.refresh()
     def dist_change(self, value):
         self.dist = self.dist_var.get()
         self.refresh()
     def size_change(self, value):
         self.size = self.size_var.get()
+        self.refresh()
+    def config(self, event):
+        self.cx, self.cy = event.width / 2, event.height / 2
         self.refresh()
     def reset(self, verts = {}, lines = set()):
         self.verts = copy.deepcopy(verts)
